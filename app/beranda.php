b2c2bd26d8b0d626d29bcb8dcf265f9ec21d8317
Indira Melinda Putri <indiraputri196@gmail.com>
2019-10-09 19:01:54 +0700

    model yang digunakan untuk menampilkan data

--- app/beranda.php
+++ app/beranda.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class beranda extends Model
 {
     protected $table = 'mahasiswa';
+    public $timestamps = false;
     protected $guarded = ['id_mahasiswa'];
     public $primaryKey = 'id_mahasiswa';
 }
